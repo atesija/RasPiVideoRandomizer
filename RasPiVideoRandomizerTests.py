8c10b48b430fad481ba1875919917268f6c11830
Anthony Tesija <antesija@gmail.com>
2017-08-05 00:17:14 -0400

    Whitelist strings tests

--- RasPiVideoRandomizerTests.py
+++ RasPiVideoRandomizerTests.py
@@ -20,4 +20,16 @@ class RasPiVideoRandomizerTests(unittest.TestCase):
     def test__video_contains_string__matching_string_and_non_matching_string__true(self):
         self.assertTrue(RasPiVideoRandomizer.video_contains_string("Scooby Doo", ["Gundam", "Doo"]))
 
+    def test__whitelist_videos__no_whitelist_strings__empty_list(self):
+    	self.assertEqual([], RasPiVideoRandomizer.whitelist_videos(["Shin Chan", "Death Note", "Monster"], []))
+
+    def test__whitelist_videos__no_matching_whitelist_strings__empty_list(self):
+    	self.assertEqual([], RasPiVideoRandomizer.whitelist_videos(["Shin Chan", "Death Note", "Monster"], ["Scooby", "Rick and Morty"]))
+
+    def test__whitelist_videos__one_video_matches_whitelist_strings__video_in_list(self):
+    	self.assertEqual(["Shin Chan"], RasPiVideoRandomizer.whitelist_videos(["Shin Chan", "Death Note", "Monster"], ["Shin", "Scooby"]))
+
+    def test__whitelist_videos__all_videos_match_whitelist_strings__video_in_list(self):
+    	self.assertEqual(["Shin Chan", "Death Note", "Monster"], RasPiVideoRandomizer.whitelist_videos(["Shin Chan", "Death Note", "Monster"], ["Shin", "Death", "Monster"]))
+
 unittest.main()
