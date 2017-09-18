e57b172d8d6baafb85a850e329db54b02eb3f92d
Anthony Tesija <antesija@gmail.com>
2017-09-17 21:47:19 -0400

    Can load channel as program arg

--- RasPiVideoRandomizer.py
+++ RasPiVideoRandomizer.py
@@ -3,6 +3,7 @@ import os
 import re
 import random
 import json
+import sys
 import BumpGenerator
 from FileFinder import get_files_of_type_from_folders
 from subprocess import Popen, PIPE
@@ -84,7 +85,11 @@ def is_video_playing():
 
 
 if __name__ == "__main__":
-    configuration_json = json.load(open("Channels/Configuration.json"))
+    json_channel = "Channels/Configuration.json";
+    if(len(sys.argv) > 1):
+        json_channel = sys.argv[1]
+        
+    configuration_json = json.load(open(json_channel))
     
     video_filetypes = [".mp4", ".avi", ".mkv", ".flv", ".ogm"]
     shows = randomize_videos(get_files_of_type_from_folders(configuration_json["folders"]["shows"], video_filetypes))
