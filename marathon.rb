f80ef4197a2dfbd70ee5954cfca4af797590222d
Anthony Tesija <antesija@gmail.com>
2019-01-04 02:55:31 -0500

    Add marathon mode

---
+++ marathon.rb
@@ -0,0 +1,18 @@
+VIDEO_LOCATION =  "/media/pi/Windfish/Videos/**/*"
+all_videos = Dir[VIDEO_LOCATION].select { |f| File.file? f }
+
+shows = ARGV
+show_filenames = []
+shows.each do |show|
+    videos = all_videos.select{ |v| v.include? show }.sort
+    show_filenames << videos
+end
+
+final_show_list = []
+while !show_filenames.empty? do
+    index = rand(0..(show_filenames.length - 1))
+    final_show_list << show_filenames[index].shift unless show_filenames[index].empty?
+    show_filenames.delete_at(index) if show_filenames[index].empty?
+end
+
+puts final_show_list 
