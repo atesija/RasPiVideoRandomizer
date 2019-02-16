52117be974838c63f9a011de21c7aee94c74dab8
Anthony Tesija <antesija@gmail.com>
2019-02-16 01:34:30 -0500

    Add old run shell scripts

---
+++ OldRandomizer/VideoPlayer.sh
@@ -0,0 +1,2 @@
+cd RasPiVideoRandomizer/RasPiVideoRandomizer/
+python ChannelEditor.py
---
+++ OldRandomizer/VideoPlayerStartup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sleep 5
+cd Desktop/RasPiVideoRandomizer/RasPiVideoRandomizer/
+python RasPiVideoRandomizer.py Channels/All.json
