361fc38aa9b359c3c061f43ea6533ad524480731
Anthony Tesija <antesija@gmail.com>
2019-02-15 22:32:32 -0800

    Put commercials between videos

--- random.rb
+++ random.rb
@@ -2,6 +2,7 @@ VIDEO_LOCATION =  "/media/pi/Windfish/Videos/**/*"
 COMMERCIAL_LOCATION = '/media/pi/Windfish/Commercials/**/*'
 BUMP_LOCATION = '/media/pi/Windfish/Bumps/**/*'
 INTRO_LOCATION = '/media/pi/Windfish/Intros/**/*'
+COMMERCIALS_PER_VIDEO = 2
 
 def get_videos_from_dir(directory)
     Dir[directory].select { |f| File.file? f }
@@ -13,32 +14,27 @@ bump_videos = get_videos_from_dir(BUMP_LOCATION)
 intro_videos = get_videos_from_dir(INTRO_LOCATION)
 
 shows = ARGV
-final_show_list = shows.empty? ? all_videos : []
+full_shows_list = shows.empty? ? all_videos : []
 shows.each do |show|
     videos = all_videos.select{ |v| v.include? show }.sort
-    final_show_list += videos
+    full_shows_list += videos
 end
-final_show_list.shuffle!
+
+full_shows_list.shuffle!
 interstitial_videos = commercial_videos + bump_videos
 interstitial_videos.shuffle!
 intro_videos.shuffle!
 
-puts "Playing: #{intro_videos[0]}"
-`lxterminal -e omxplayer '#{intro_videos[0]}' -b -o hdmi`
-sleep 1
-final_show_list.each do |show|
-    while `ps ax`.include?('omxplayer') do
-        sleep 1
-    end
+final_video_list = full_shows_list.zip(interstitial_videos.each_slice(COMMERCIALS_PER_VIDEO).to_a).flatten.compact
+final_video_list.unshift(intro_videos[0]) if !intro_videos.empty?
+
+final_video_list.each do |show|
     puts "Playing: #{show}"
     `lxterminal -e omxplayer "#{show}" -b -o hdmi`
     sleep 1
     
     while `ps ax`.include?('omxplayer') do
-    sleep 1
+        sleep 1
     end
-    puts "Playing: #{interstitial_videos[0]}"
-    `lxterminal -e omxplayer '#{interstitial_videos[0]}' -b -o hdmi`
-    sleep 1
 end
 
